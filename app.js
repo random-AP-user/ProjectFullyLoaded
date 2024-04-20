a589f897051ff2b23c96411a34a0c8d3a4509ec2
random-AP-user <160720879+random-AP-user@users.noreply.github.com>
2024-04-20 19:35:06 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const os = require('os');
 const fs = require('fs');
-const https = require("https");
+const http = require('http');
 const express = require("express");
-const session = require('cookie-session');
+const session = require("cookie-session");
 const multer = require("multer");
 const flash = require("connect-flash");
 const bodyParser = require("body-parser");
@@ -16,31 +16,12 @@ const webpush = require("web-push");
 dotenv.config({ path: './.env' });
 
 const app = express();
+const server = http.createServer(app);
 
-const sslServer = https.createServer(
-  {
-    key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
-    cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem')),
-  },app
-);
+const io = require("socket.io")(server);
 
-const io = require("socket.io")(sslServer);
-// too many hours wasted 
-// todo
-//! video upload
-//? change image
-//? change password
-//? list of players
-//?// active count
-//?// delete all active bounty's
-//!// push notification
-
-
-
-// storage engine
 const storage = SharpMulter({
   destination: (req, file, cb) => cb(null, "./public/images"),
-
   imageOptions: {
     fileFormat: "webp",
     quality: 80,
@@ -52,9 +33,7 @@ const storage = SharpMulter({
   },
 });
 
-const upload = multer({
-  storage: storage
-});
+const upload = multer({ storage: storage });
 const db = mysql.createConnection({
   host: process.env.HOST,
   user: process.env.USER,
@@ -64,7 +43,6 @@ const db = mysql.createConnection({
 
 db.connect((err) => {
   if (err) throw err;
-  console.log("connection to db succeeded");
 });
 
 app.use(express.static(path.join(__dirname, "public")));
@@ -72,7 +50,6 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// Middleware for session management
 app.use(session({
   secret: process.env.SESSION_SECRET,
   cookie: { maxAge: 24 * 60 * 60 * 1000 },
@@ -88,12 +65,11 @@ io.on("connection", (socket) => {
     console.log(`Received message from client: ${message}`);
     socket.broadcast.emit("serverMessage", `Server broadcast: ${message}`);
   });
-
 });
 
 app.get("/", (req, res) => {
   q = "SELECT u.username, u.image, b.price FROM users u LEFT JOIN bounty b ON u.userID = b.userID WHERE u.userID = b.userID ORDER BY b.price DESC";
-  db.query(q, (err, rows, field) => {
+  db.query(q, (err, rows) => {
     if (err) throw err;
     res.render("home.ejs", { users: rows, username: req.session.username, admin: req.session.admin });
   });
@@ -108,15 +84,15 @@ app.get("/register", (req, res) => {
 });
 
 app.get("/logout", (req, res) => {
-  res.clearCookie('session');
-  res.redirect("/")
+  req.session = null;
+  res.redirect("/");
 });
 
 app.post("/loginuser", async (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
   q = "SELECT * FROM users WHERE username = ?";
-  db.query(q, [username], async (err, rows, field) => {
+  db.query(q, [username], async (err, rows) => {
     if (err) {
       res.status(500).send('Error retrieving user');
       return;
@@ -143,7 +119,7 @@ app.get('/profile', (req, res) => {
     res.redirect("/login");
   } else {
     q = "SELECT secretcode , price FROM bounty b WHERE userID = (SELECT userID FROM users u WHERE userID = ?)";
-    db.query(q, [req.session.userID], (err, rows, field) => {
+    db.query(q, [req.session.userID], (err, rows) => {
       res.render('profile.ejs', { username: req.session.username, image: req.session.image, id: req.session.userID, query: rows[0] });
     });
   }
@@ -158,10 +134,8 @@ app.post("/registeruser", upload.single('userimage'), (req, res) => {
   const userimage = req.file.filename;
 
   q = "SELECT * FROM users WHERE username = ?";
-  db.query(q, [username], (err, rows, field) => {
-    if (err) {
-      throw err;
-    }
+  db.query(q, [username], (err, rows) => {
+    if (err) throw err;
 
     if (rows.length > 0) {
       console.log("Duplicate found");
@@ -181,7 +155,7 @@ app.post("/registeruser", upload.single('userimage'), (req, res) => {
           throw hashErr;
         }
         q = "INSERT INTO users(username, password, image) VALUES (?, ?, ?)";
-        db.query(q, [username, hashedPassword, userimage], (err, rows, fields) => {
+        db.query(q, [username, hashedPassword, userimage], (err, rows) => {
           if (err) {
             throw err;
           }
@@ -209,7 +183,7 @@ app.post("/clameconfirm", (req, res) => {
   } else {
     const bountypass = req.body.bountypass;
     q = "SELECT * FROM `bounty` WHERE secretcode = ?";
-    db.query(q, [bountypass], (err, rows, fields) => {
+    db.query(q, [bountypass], (err, rows) => {
       if (err) throw err;
       const userID = req.session.userID;
       const killedID = rows[0].userID;
@@ -217,9 +191,9 @@ app.post("/clameconfirm", (req, res) => {
       const bountyID = rows[0].bountyID;
       if (rows.length > 0 && userID != killedID) {
         q2 = "INSERT INTO collectedbounty (userID, killedID, price) VALUES (?, ?, ?);";
-        db.query(q2, [userID, killedID, price], (err, rows, fields) => {
+        db.query(q2, [userID, killedID, price], (err, rows) => {
           q3 = "DELETE FROM bounty WHERE bountyID = ?";
-          db.query(q3, [bountyID], (err, rows, fields) => {
+          db.query(q3, [bountyID], (err, rows) => {
             if (err) throw err;
             io.sockets.emit("bountyUpdate");
             res.redirect("/");
@@ -240,9 +214,9 @@ app.get("/collected", (req, res) => {
   } else {
     userID = req.session.userID;
     q = "SELECT price, (SELECT username FROM users u WHERE u.userID = c.killedID ) AS victim FROM collectedbounty c WHERE c.userID = ? AND collected = 0;";
-    db.query(q, [userID], (err, rows, fields) => {
+    db.query(q, [userID], (err, rows) => {
       q2 = "SELECT price, (SELECT username FROM users u WHERE u.userID = c.killedID ) AS victim FROM collectedbounty c WHERE c.userID = ? AND collected = 1;";
-      db.query(q2, [userID], (err2, rows2, fields2) => {
+      db.query(q2, [userID], (err2, rows2) => {
         res.render("collectedbounty.ejs", { notcollected: rows, iscollected: rows2 });
       });
     });
@@ -254,14 +228,13 @@ app.post("/addbounty", (req, res) => {
   const price = req.body.price;
 
   q = "select username, image from users where userID = ?";
-  db.query(q, [userID], (err, row, field) => {
+  db.query(q, [userID], (err, row) => {
     message = `Bounty on ${row[0].username}\nReward: ৳${req.body.price}`;
     fullUrl = `${req.protocol}://${req.get('host')}/images/`;
     icon = `${fullUrl}${row[0].image}`;
-    //? if future me forgets the DUAL creates an empty row and column tldr DUAL = (select 1)
     const number = Math.floor(Math.random() * 10001);
     q2 = "INSERT INTO `bounty` (`userID`, `price`, `secretcode`) VALUES (?, ?, LPAD(FLOOR(RAND() * 10000), 4));";
-    db.query(q2, [userID, price], (err2, row2, field2) => {
+    db.query(q2, [userID, price], (err2, row2) => {
       if (err) throw err;
       io.sockets.emit("bountyUpdate");
       sendPushNotification("ALERT: New Bounty", message, icon);
@@ -273,7 +246,7 @@ app.post("/changebounty", (req, res) => {
   const bountyID = req.body.bountyID;
   const price = req.body.price;
   q = "UPDATE bounty SET price = ? WHERE bountyID = ?";
-  db.query(q, [price, bountyID], (err, row, field) => {
+  db.query(q, [price, bountyID], (err, row) => {
     if (err) throw err;
     io.sockets.emit("bountyUpdate");
     res.redirect("/admin");
@@ -283,7 +256,7 @@ app.post("/changebounty", (req, res) => {
 app.post("/deletebounty", (req, res) => {
   const bountyID = req.body.bountyID;
   q3 = "DELETE FROM bounty WHERE bountyID = ?";
-  db.query(q3, [bountyID], (err, rows, fields) => {
+  db.query(q3, [bountyID], (err, rows) => {
     if (err) throw err;
     io.sockets.emit("bountyUpdate");
     res.redirect("/admin");
@@ -293,7 +266,7 @@ app.post("/deletebounty", (req, res) => {
 app.post("/deleteAllBounty", (req, res) => {
   const bountyID = req.body.bountyID;
   q3 = "DELETE FROM bounty WHERE bountyID > 0";
-  db.query(q3, [bountyID], (err, rows, fields) => {
+  db.query(q3, [bountyID], (err, rows) => {
     if (err) throw err;
     io.sockets.emit("bountyUpdate");
     res.redirect("/admin");
@@ -306,12 +279,12 @@ app.get("/admin", (req, res) => {
     res.redirect("/");
   } else {
     q1 = "SELECT u.userID, u.username, u.image FROM users u WHERE u.userID NOT IN (SELECT b.userID FROM bounty b);";
-    db.query(q1, (err1, rows1, fields1) => {
+    db.query(q1, (err1, rows1) => {
       if (err1) {
         throw err1;
       }
       q2 = "SELECT u.userID, u.username, u.image, b.price, b.bountyID FROM users u JOIN bounty b ON u.userID = b.userID";
-      db.query(q2, (err2, rows2, fields2) => {
+      db.query(q2, (err2, rows2) => {
         if (err2) {
           throw err2;
         }
@@ -324,20 +297,18 @@ app.get("/admin", (req, res) => {
 
 app.get("/getUpdatedBounties", (req, res) => {
   q = "SELECT u.username, u.image, b.price FROM users u LEFT JOIN bounty b ON u.userID = b.userID WHERE u.userID = b.userID ORDER BY b.price DESC";
-  db.query(q, (err, rows, fields) => {
+  db.query(q, (err, rows) => {
     if (err) {
-      console.error('Error executing SQL query:', err);
       res.status(500).json({ error: 'Internal Server Error' });
       return;
     }
-
     res.json({ updatedBountyContent: rows });
   });
 });
 
 app.get("/userslist", (req, res) => {
   q = "SELECT username, image FROM users ORDER BY username";
-  db.query(q, (err, rows, fields) => {
+  db.query(q, (err, rows) => {
     if (err) {
       throw err;
     }
@@ -345,7 +316,6 @@ app.get("/userslist", (req, res) => {
   });
 });
 
-// active users
 function test() {
   io.sockets.emit("activeUsers");
 }
@@ -355,8 +325,6 @@ app.get("/refreshUsers", (req, res) => {
 });
 
 setInterval(test, 2000);
-
-// web push notif
 
 const publicVapidKey = process.env.publicKey;
 const privateVapidKey = process.env.privateKey;
@@ -403,7 +371,6 @@ async function sendPushNotification(title, message, icon) {
       await webpush.sendNotification(subscription, JSON.stringify(payload));
     } catch (error) {
       if (error.statusCode === 410) {
-        console.log("Subscription is no longer valid, remove it from your list \n" + JSON.stringify(subscription));
         subscribers.splice(i, 1);
         i--;
       } else {
@@ -412,13 +379,7 @@ async function sendPushNotification(title, message, icon) {
     }
   }
   fs.writeFileSync("./subscribers.json", JSON.stringify(subscribers, null, 2));
-  console.log("finnished sending all notification");
 }
-
-
-
-
-
 
 function getIPAddress() {
   let interfaces = os.networkInterfaces();
@@ -434,18 +395,9 @@ function getIPAddress() {
   return '0.0.0.0';
 }
 
-// defender firwerall
-// telenet
-//    local ip | external port | internal port | protocol
-//   laptop ip |    79-81      |  server port  |  Both
-// inbound = server port
-// outbound = port 80
-//ipethernet = "192.168.0.166";
-
-port = process.env.PORT || 10000;
-// ipwifi = getIPAddress();
+port = process.env.PORT || 10000 ;
 ipwifi = "127.0.0.1";
 
-sslServer.listen(port, () => {
-  console.log(`Server is running on https://${ipwifi}:${port}`);
+server.listen(port, () => {
+  console.log(`Server is running on http://${ipwifi}:${port}`);
 });
